f6ab34d3f671a5018947a9b752aaa59d1bae8e1c
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-22 22:12:13 +0900

    feat: 상단 탭 메뉴 구현

---
+++ src/index.js
@@ -0,0 +1,21 @@
+class VendingMachine {
+	constructor() {
+		this.render()
+	}
+
+	render() {
+		document.querySelector('#app').insertAdjacentHTML('afterbegin', this.getTapTemplate())
+	}
+	getTapTemplate() {
+		return `        
+    <h2>자판기 만들기🧃</h2>
+    <nav id='nav-menu'>    
+    <button id='product-add-menu'>상품 관리</button>
+    <button id='vending-machine-manage-menu'>잔돈 충전</button>
+    <button id='product-purchase-menu'>상품 구매</button>
+    </nav>
+    `
+	}
+}
+
+new VendingMachine()
